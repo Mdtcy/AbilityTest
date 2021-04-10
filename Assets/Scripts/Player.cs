84390bc67efe1db287b21d52dc3aaae910039710
Kim <tktetb@163.com>
2021-04-10 15:36:42 +0800

    test

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using EGamePlay.Combat;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public CombatEntity CombatEntity;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
